b6fa6267e9050d5a6976e332c28be704f2761c1f
Valentin Bootz <valentinbootz@7b8131a2-c103-47b9-a872-9a338730da91.fritz.box>
2020-01-29 00:14:47 +0100

    Fix column widths

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a85c3dbdfacde1085bcf";
+/******/ 	var hotCurrentHash = "2c83c95fdfe9e3f20ad4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
